ed0734251571a3a36175343b81f35b051b6c16f9
katelynsumimoto <jilleb@jakku.edu>
2020-04-21 15:18:34 -1000

    Add requirement 1

--- client/javascripts/gallery.js
+++ client/javascripts/gallery.js
@@ -1,15 +1,18 @@
 // jshint esversion: 6
 
-let main = function(){
+let controller = function() {
 
-    let prefixURL = "http://api.flickr.com/services/feeds/photos_public.gne?tags=";
-    let suffixURL = "&format=json&jsoncallback=?";
-    //get value entered by user from textbox
-    let flickrTag = $("input").???();
-    let requestURL = prefixURL + flickrTag + suffixURL;
-
-    //clear old photos
-    $(".photos").???("");
+  let prefixURL = "https://api.flickr.com/services/feeds/photos_public.gne?tags=";
+  let suffixURL = "&format=json&jsoncallback=?";
+  //get value entered by user from textbox
+  //let flickrTag = document.querySelector("input[type=text]").value;
+  let flickrTag = $("input").val();
+  console.log(flickrTag);
+  let requestURL = prefixURL + flickrTag + suffixURL;
+  console.log(requestURL);
+  //clear old photos
+  //document.querySelector(".photos").innerHTML = "";
+  $(".photos").html("");
 
   $.getJSON(requestURL, function(flickrResponse) {
     flickrResponse.items.forEach(function(item, index) {
@@ -37,4 +40,8 @@ let main = function(){
 };
 
 
-$(document).ready(main);
+//$(document).ready(controller);
+window.addEventListener("load", function() {
+  let button = document.querySelector("button");
+  button.addEventListener("click", controller);
+});
