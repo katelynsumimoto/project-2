1684ea1892b8cf25c55c955b700c9828d9cacd96
katelynsumimoto <jilleb@jakku.edu>
2020-04-23 15:44:48 -1000

    Add requirement 3 and 4

--- client/javascripts/functions.js
+++ client/javascripts/functions.js
@@ -14,3 +14,58 @@ let containsDuplicates = s => {
   }
   return false;
 };
+
+//Req 3-a
+let revWord1 = (str) => {
+  let arr = str.split(" ");
+  let result = " ";
+  for (var i = 0; i < str.length; i++) {
+    result = str[i] + result;
+  }
+  return result;
+};
+let revWords1 = (sent) => {
+  let arr = sent.split(" ");
+  let result = " ";
+  for (var i = 0; i < arr.length; i++) {
+    result = revWord1(arr[i]) + result;
+  }
+  return result;
+};
+
+//Req 3-b
+let revWord2 = (str) => {
+  let arr = str.split(" ");
+  let result = " ";
+  for (var i = 0; i < str.length; i++) {
+    result = str[i] + result;
+  }
+  return result;
+};
+let revWords2 = (sent) => {
+  let arr = sent.split(" ");
+  let result = " ";
+  arr.forEach((char) => {
+    result = revWord2(char) + result;
+  });
+  return result;
+};
+
+
+//Req 3-c
+let revWord3 = (str) => {
+  let arr = str.split(" ");
+  let result = " ";
+  for (var i = 0; i < str.length; i++) {
+    result = str[i] + result;
+  }
+  return result;
+};
+let revWords3 = (sent) => {
+  let arr = sent.split(" ");
+  let result = " ";
+  for (char of arr) {
+    result = revWord3(char) + result;
+  }
+  return result;
+};
